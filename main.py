731934daf3839cd99eafe2065411f5d6741c8924
sc0der <siriusproger@gmail.com>
2021-06-25 09:10:11 +0500

    initialization the project

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from notification import SendNotification
 
 @click.command()
 @click.option("--send_by", default="mail", prompt="Укажите способ отправки уведомление",help="Укажите способ отправки уведомление о совершении покупки 'mail' по почте и 'sms' по номеру телефона ")
-# @click.option("--/help", help="Укажите способ отправки уведомление о совершении покупки")
 
 def main(send_by):
     notify = SendNotification(customer_id=1, purchase_id=1)
